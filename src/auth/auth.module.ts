8a5162c37ca21706edfdce13ccb656e6b96d0aa2
Azat Saaryan <azat.saaryan2022@gmail.com>
2025-09-24 12:41:40 +0400

    login functionality went successfuly

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule,
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '7d' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
-// import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(data: { name: string; email: string; password: string }) {
     const existingUser = await this.userService.findByEmail(data.email);
@@ -21,6 +24,22 @@ export class AuthService {
     });
     return {
       user: { id: newUser.id, name: newUser.name, email: newUser.email },
+    };
+  }
+
+  async login(data: { email: string; password: string }) {
+    const user = await this.userService.findByEmail(data.email);
+    if (!user) throw new UnauthorizedException();
+
+    const isPasswordValid = await bcrypt.compare(
+      data.password,
+      user.password_hash,
+    );
+    if (!isPasswordValid) throw new UnauthorizedException();
+
+    const payload = { sub: user.id, username: user.name };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
     };
   }
 }
