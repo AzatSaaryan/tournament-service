e6a6620edf36c1721de453664f2a9dbfd53159a1
Azat Saaryan <azat.saaryan2022@gmail.com>
2025-09-24 09:59:44 +0400

    finished database.service

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Pool, PoolClient, QueryResult, QueryResultRow } from 'pg';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService implements OnModuleInit, OnModuleDestroy {
+  private pool: Pool;
+
+  async onModuleInit() {
+    this.pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    const client = await this.pool.connect();
+    client.release();
+    console.log('Database connected successfully');
+  }
+
+  async onModuleDestroy() {
+    await this.pool.end();
+    console.log('❌ Database connection closed');
+  }
+
+  async query<T extends QueryResultRow>(
+    text: string,
+    params?: any[],
+  ): Promise<QueryResult<T>> {
+    return this.pool.query<T>(text, params);
+  }
+
+  async getClient(): Promise<PoolClient> {
+    return this.pool.connect();
+  }
+}
