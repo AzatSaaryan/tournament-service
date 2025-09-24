f3d325907508563c64d01344b8d057475c91de40
Azat Saaryan <azat.saaryan2022@gmail.com>
2025-09-24 10:42:31 +0400

    finished user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { UserRow } from './users.types';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async findByEmail(email: string): Promise<UserRow | undefined> {
+    return this.repository.findByEmail(email);
+  }
+
+  async findById(id: number): Promise<UserRow | undefined> {
+    return this.repository.findById(id);
+  }
+
+  async createUser(data: {
+    name: string;
+    email: string;
+    passwordHash: string;
+  }): Promise<UserRow> {
+    return this.repository.createUser(data);
+  }
+}
